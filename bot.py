9f1fbc0a388d70f117ae0933e08003750e4856ec
whiteo83 <32384531+whiteo83@users.noreply.github.com>
2017-10-03 13:52:34 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ import urllib
 # Example of your code beginning
 #           Config vars
 TOKEN = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
+#some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
 # If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
